a6da8847874cf47e780cfc90e7acc41a0450e1ae
artem-kondratew <artemkondratev5@gmail.com>
2024-05-11 22:44:29 +0300

    change aligned depth source

--- rtabmap_examples/launch/realsense_d435i_color.launch.py
+++ rtabmap_examples/launch/realsense_d435i_color.launch.py
@@ -2,7 +2,7 @@
 #   A realsense D435i
 #   Install realsense2 ros2 package (ros-$ROS_DISTRO-realsense2-camera)
 # Example:
-#   $ ros2 launch realsense2_camera rs_launch.py enable_gyro:=true enable_accel:=true unite_imu_method:=1 enable_sync:=true
+#   $ ros2 launch realsense2_camera rs_launch.py enable_gyro:=true enable_accel:=true unite_imu_method:=1 enable_sync:=true align_depth.enable=true
 #
 #   $ ros2 launch rtabmap_examples realsense_d435i_color.launch.py
 
@@ -16,14 +16,14 @@ def generate_launch_description():
           'frame_id':'camera_link',
           'subscribe_depth':True,
           'subscribe_odom_info':True,
-          'approx_sync':False,
+          'approx_sync':True,
           'wait_imu_to_init':True}]
 
     remappings=[
           ('imu', '/imu/data'),
           ('rgb/image', '/camera/color/image_raw'),
           ('rgb/camera_info', '/camera/color/camera_info'),
-          ('depth/image', '/camera/realigned_depth_to_color/image_raw')]
+          ('depth/image', '/camera/aligned_depth_to_color/image_raw')]
 
     return LaunchDescription([
 
